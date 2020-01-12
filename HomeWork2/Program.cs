56a78ed5c3073a7198646d94375503e5ee5c59c4
amberinis <gintaras.cilcius@gmail.com>
2020-01-12 21:22:51 +0200

    Function CompareArrays

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -15,7 +15,7 @@ namespace HomeWork2
         static int[] NumberToArray(int inputNumber)
         {
             int[] inputArray = new int[Convert.ToString(inputNumber).Length];
-            for (int i = inputArray.Length - 1; i >=0; i--)
+            for (int i = inputArray.Length - 1; i >= 0; i--)
             {
                 inputArray[i] = inputNumber % 10;
                 inputNumber = inputNumber / 10;
@@ -34,6 +34,27 @@ namespace HomeWork2
                 }
             }
             return true;
+        }
+        static bool CompareArrays(int[] inputArray1, int[] inputArray2)
+        {
+            if (inputArray1.Length == inputArray2.Length)
+            {
+                int[] tempArray1 = inputArray1;
+                int[] tempArray2 = inputArray2;
+                Array.Sort(tempArray1);
+                Array.Sort(tempArray2);
+                for (int i = 0; i < tempArray1.Length; i++)
+                {
+
+                    if (tempArray1[i] != tempArray2[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+
         }
     }
 }
