3a2d60ba7b6396afbfbe7b43ae8ac6102b538ec0
amberinis <gintaras.cilcius@gmail.com>
2020-01-12 20:30:00 +0200

    Function NumbersToArray

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -6,7 +6,21 @@ namespace HomeWork2
     {
         static void Main()
         {
+            foreach (var item in NumberToArray(1234567))
+            {
+                Console.WriteLine(item);
+            }
             Console.WriteLine();
+        }
+        static int[] NumberToArray(int inputNumber)
+        {
+            int[] inputArray = new int[Convert.ToString(inputNumber).Length];
+            for (int i = inputArray.Length - 1; i >=0; i--)
+            {
+                inputArray[i] = inputNumber % 10;
+                inputNumber = inputNumber / 10;
+            }
+            return inputArray;
         }
     }
 }
